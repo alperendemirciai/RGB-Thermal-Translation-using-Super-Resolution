0d1c971f49c78f772eb944c6cdae6d3c831bbde2
Alperen <60541504+alperendemirciai@users.noreply.github.com>
2025-05-10 13:57:25 +0300

    added mps device support

--- create_sr_images.py
+++ create_sr_images.py
@@ -10,7 +10,14 @@ from glob import glob
 
 def apply_super_resolution_to_all_images():
     # Set up device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = None
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+
     print(f"Using device: {device}")
 
     # Directory containing input images
